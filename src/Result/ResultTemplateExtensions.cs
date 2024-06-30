ad4572225019a857c6eb5bbc99b9d40d94617709
ErgodicMage <ErgodicMage@gmail.com>
2024-06-29 21:37:47 -0500

    Added ResultTemplateExtension with new Convert function

---
+++ src/Result/ResultTemplateExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ErgodicMage.Result;
+
+public static class ResultTemplateExtensions
+{
+    public static Result<T> Convert<T, U>(this Result<U> result)
+    where T : class, U
+    where U : class
+    => result.Success switch
+    {
+        true => Result<T>.Ok((result.Value as T)!),
+        _ => Result<T>.Error(result.ErrorMessage)
+    };
+}
